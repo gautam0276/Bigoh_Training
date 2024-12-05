2b9295eb8a9d2e224114b10337a5527b5108fe40
gautam0276 <gautam0276@gmail.com>
2024-12-05 11:16:07 +0530

    feat: day-1-tasks

---
+++ Java Script/day1.js
@@ -0,0 +1,115 @@
+
+const name="Gautam";
+
+
+
+let m;
+console.log(m)
+
+let n=null;
+console.log(n)
+// Arithmetic Operators( +,-,*,/,%)
+console.log(1+2) ;
+
+console.log(2*8);
+console.log(6-8);
+
+
+let a=2;
+let b=4;
+console.log(b/a);
+
+// var value ko update krta jata h line to line 
+var x=5;
+var x=20;
+var x=55;
+console.log(x)
+
+// age ek variable ko let krlia to dubara usse let nhi kr skte
+
+let c=6;
+let s=7;
+
+console.log(a);
+
+// const ki value change nhi ho skte
+const y=5;
+const z=9;
+
+console.log(y);
+
+// Operators +,-,*,/
+
+let sohitage=20;
+let mohitage=17;
+
+console.log(sohitage + mohitage);
+
+// Expressions ka mtlb h ki (variable declare krke kisi variable m hi uska ans krna)
+// like c=a+b*d
+
+var d=8;
+var e=4;
+var f=3;
+var total;
+
+console.log(total=d+e*f);
+
+let num=9958402557;
+
+
+
+var fullName="Gautam Sharma";
+console.log(fullName);
+
+let isPass= true;
+console.log(isPass);
+
+
+//  object
+// key values
+
+const employee = {
+fullName : "Harshit",
+age:23,
+num:1234567890,
+design: "development",
+
+};
+console.log(employee);
+
+// Key: value pair
+
+const product={
+brand : "parkerJotter",
+rating:4,
+colour:"black",
+price:270,
+discount: 5% "off", 
+
+}
+console.log(product);
+
+const h=5;
+const i=2;
+
+console.log("h++=" , h+h);
+console.log(" h % i = " , h % i );
+
+console.log("h**i=" , h**i);
+
+// comparision Operator
+console.log(22===32)
+
+console.log(5!=5)
+
+// Logical Operator
+console.log( 4<5 && 8<9)
+
+console.log(2<5 || 8>10)
+
+console.log( !(2<1) )
+
+
+
+
---
+++ Java Script/day2.js
@@ -0,0 +1,17 @@
+// agr m ek if codition hi daaltey h or vo hi true hogi to aagey statement read hi nhi kregi
+
+const age=36;
+if(age<=60){
+    console.log("Not a Senior Citizen" , age);
+}
+
+// if else ( agr if ki condition true nhi hogi to automatically else print ho jyga)
+const num=25;
+if(num%2==0){
+    console.log("even")
+}
+else{
+    console.log("odd");
+}
+
+console.log(num);
