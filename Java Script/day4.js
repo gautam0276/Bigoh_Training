13a5268c5441ae3152cb7d455f1162f9fd426960
gautam0276 <gautam0276@gmail.com>
2024-12-10 10:42:22 +0530

    feat arrays and objects

---
+++ Java Script/day4.js
@@ -0,0 +1,121 @@
+// // object k andr function ko define krna ( object jo h vo card ko as a reference call kr rha h)
+
+// let obj={
+// name:"gautam",
+// code:490,
+// desig: "development",
+// company: "Bigoh",
+
+// card: function(){
+//     console.log("app sbko shadi m zaroor aana h")
+// }
+// }
+
+// console.log(obj)
+// obj.card()
+
+// let arr=[1,2,3,4,5]
+// arr.push('babbar')    // push ek element add kr deta h last m
+// arr.pop()               // pop ek element hata deta h last m
+// arr.shift()             // shift ek element hata deta h starting m
+// arr.unshift('krishna')  // unshift ek element add kr deta h starting m
+// arr.push('sharma ji')
+// arr.push('45')
+// arr.unshift('start')
+// arr.splice(2,2,'element remove')  // splice ek element ko remove  , insert and or uski  jagah kch or bhi insert kr skta h
+// //(2  is ki konse index se start krna h, kitne index remove krne h , kya insert krna h ) 
+// console.log(arr)
+// console.log(arr.slice(2,4))         // slice elements ko middle se seprate kr skta h but last one coount nhi hoga 
+
+
+
+// let arr=[1,2,3]
+
+// function map(number){
+// let ans=map.arr
+//     return number*number
+// }
+// console.log(map.arr)
+
+
+//  filter
+
+// let arr=[10,85,66,49,23,45,40]
+// let evenArr= arr.filter((number) => {
+//         if(number%2 === 0){
+//             return true
+//         }
+//         else{
+//             return false
+//         }
+//     })
+
+//     console.log(evenArr)
+
+
+    //  if we print both the numbers in array (even add odd)
+    
+// let evenArr= arr.filter((number) =>number%2 ===0)
+// let oddArr=arr.filter((number) => number%2 !==0)
+
+// console.log(evenArr)
+// console.log(oddArr)
+
+//  if any case we print string and number different
+// let arr=[10,85, "rajesh", "mohit" , "sohit", 23,45,40]
+
+// let ans=arr.filter((value) =>{
+//     if(typeof(value) === 'string')
+//         return true
+//     else
+//     return false    
+// })
+// console.log(ans)
+
+//  reduce is used to use mathematical expression 
+
+// let arr=[10,30,40,50]
+
+// let ans=arr.reduce((acc,red) =>{
+//     return acc+red
+// }, 0)
+
+
+// console.log(ans)
+
+// sorting (in ascending order)
+// let arr=[85,53,22,10,30,90,50]
+
+// arr.sort()
+// console.log(arr)
+
+// sorting in decending order
+// let arr=[85,53,22,10,30,90,50]
+
+// arr.sort()
+// console.log(arr)
+// console.log(arr.indexOf(53))  // find index of the element
+
+// console.log(arr.length) //find length of array
+
+// let arr=[85,53,22,30,90,50]
+
+// arr.forEach((value,index) => {
+//     console.log("Number :" , value, "Index :" , index)
+// })
+
+
+// console.log(arr.includes(22,2))  //include ka mtlb vo element ya string us array m h ya nhi
+
+
+
+
+
+
+// Map
+// closoure
+// for each ( objects k value or index ko seprate)
+// sort in decending order
+// false falsy
+// objects MDM3
+
---
+++ Java Script/day5.js
@@ -0,0 +1,261 @@
+// let a=5;
+// a=10
+// console.log(a)
+
+// const b=2    isme error aa jata h again b declare krne p
+// b=4
+// console.log(b)
+
+// var c=7
+// c=6
+// console.log(c)
+
+// Datatypes
+
+// let name="gautam"
+// console.log(name)
+// console.log(typeof(name))
+
+// let age=22
+// console.log(age)
+// console.log(typeof(age))
+
+// let age1=22
+// let isSenior=age1<=60
+// console.log(isSenior)
+
+
+
+// let x
+// console.log(x)   //undefined
+
+// let y="null";
+// console.log(y)
+
+// let bigInt= 1325489464865164943165654684646465464354684n
+// console.log(bigInt)
+
+// // objects
+// let obj={
+//     "first name" : "gautam",
+//     age:23,
+//     hobbies : "travelling", 
+// }
+// console.log(obj)
+
+
+// // Arrays
+
+// let arr=[9,6,5,1,2,3,4,5,6]
+// console.log(arr)
+// arr.sort()
+// console.log(arr)
+
+// let fruits=["apple","mango","litchi", "Guvava"]
+// console.log(fruits)
+
+
+// // functions
+
+// function greet(name){
+//     return "Hii Everyone "+ name
+// }
+// console.log(greet(" Good Morning"))
+
+
+// // Arithmetic operators
+
+// let a=5
+// let b=3
+
+// let sum=a+b;
+// console.log(sum)
+
+// let minus=a-b
+// console.log(minus)
+
+// let multi=a*b
+// console.log(multi)
+
+// let div=a/b
+// console.log(div)
+
+// let module=a%b==0
+// console.log(module)
+
+// console.log(a+1)
+// console.log(a-1)
+// console.log(a*2)
+
+
+// // assignment operator
+
+// let name1="gautam"
+// let age2="22"
+
+// console.log(name1=="gautam")
+
+// console.log(name1==="gautam")
+// console.log(age2 === "22")
+
+// let age3=22
+// if(age>=18){
+//     console.log("adult")
+// }
+// else{
+//     console.log("under age")
+// }
+
+
+// let myAge=29
+// let isAnAdult=true
+// if(age>=18 && isAnAdult)
+// {
+//     console.log("go to trip")
+// }
+// else{
+//     console.log("not go to picnic")
+// }
+
+
+// conditional Statement
+// if, else if, else
+
+// let temp=44
+
+// if(temp>30)
+// {
+//     console.log("its hot outside ")
+// }
+// else if(temp>20){
+//     console.log("its warn outside")
+// }
+// else
+// {
+//     console.log("its cold outside")
+
+
+// Switch statement
+
+// let colour=3
+
+// switch (colour){
+// case 1 : 
+// console.log(" pink")
+// break ;
+// case 2 :
+//     console.log("yellow")
+// break ;
+// case 3:
+//     console.log("blue")
+//     break
+// case 4:
+//     console.log("green")
+// break ;
+// default:
+//     console.log("error")
+// }
+
+
+
+// logical operator and statements
+
+// let marks=50
+// let goToTrip=true
+// let buyCar=true
+
+// if(marks>=75 && goToTrip || marks>=75 && buyCar){
+//     console.log("I am passed")
+// }
+// else{
+//     console.log("I am fail")
+// }
+
+
+// let num=20
+// for(let i=1;i<=num;i++){
+//     console.log(i)
+// }
+
+
+// let obj={
+//     gautam:45000,
+//     shiv:52000,
+//     ritesh:60000,
+//     gaurav:40000
+
+// }
+// for(let string in obj){
+// console.log("the salary of "+ string + " is " + obj[string])
+// } 
+
+// for(let string of "gautam"){
+//     console.log(string)
+// }
+
+
+// function name(fname,lname){
+//     console.log("hello " + fname + " " +lname)
+// }
+// (name("rohit","sharma"))
+
+// function sum(a=20,b=32){
+//     console.log(a+b)
+// }
+// sum()
+
+
+// const sum=(name,age)=>{
+//     if(name=="gautam" || age>18 )
+//     return true
+// else
+//     return false
+
+// }
+// console.log(sum("ritesh",5))
+
+
+// const obj={
+//     name :"gautam",
+//     age:23,
+//     hobbies : "travelling"   
+// }
+// for(let key in obj){
+//     console.log(obj[key])
+// }
+
+// const arr=[1,8,6,3,7,5]
+// for(let value of arr){
+//     console.log(value)
+// }
+
+
+// // arrow function
+// const printName=(fname,lname)=>{
+//     console.log(fname +" " +lname)
+// }
+// printName("gautam","sharma")
+
+
+
+// let string="travelling"
+// for(let value of string){
+//     console.log(value)
+// }
+
+
+
+// const obj={
+//         name :"gautam",
+//         age:23,
+//         hobbies : "travelling"   
+//     }
+//     for(let value in obj){
+//         console.log(obj[value])
+//     }
+
+// const arr=[5,8,9,3,6,7,1,4]
+// for(let value of arr){
+//     console.log(value,indexof.arr)
+// }
+
