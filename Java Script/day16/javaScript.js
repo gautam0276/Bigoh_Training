ef5e445b1f63f180eb9f8ee9494719260459f35d
gautam0276 <gautam0276@gmail.com>
2024-12-24 00:20:45 +0530

    DOM

---
+++ Java Script/day16/javaScript.js
@@ -0,0 +1,52 @@
+// function changeText() {
+//   //   document.getElementById("heading").innerText = "My Website";
+//   //   document.getElementById("header").style.backgroundColor = "black";
+//   // }
+//   // alert("js working");
+//   //   document.getElementsByTagName('body');
+
+//   document.getElementById("heading").innerText = "Hello World";
+//   document.createElement("New Website")
+// }
+
+
+
+
+
+
+
+// function changeText() {
+  //   document.getElementById("heading").innerText = "My Website";
+  //   document.getElementById("header").style.backgroundColor = "black";
+  // }
+  // alert("js working");
+  //   document.getElementsByTagName('body');
+
+//   document.getElementById("heading").innerText = "Hello World";
+//   document.createElement("New Website");
+  
+// document.getElementById("image").
+//     document.getElementById("img").src =
+//       "https://media.geeksforgeeks.org/wp-content/uploads/20190529122826/bs11.png";
+  
+// }
+
+const button=document.getElementById("my button")
+button.addEventListener("click",function(){
+    alert("button clicked")
+});
+const element=document.querySelector("#modifiedCircle")
+function changeCircleColor(){
+    // alert(element)
+}
+element.addEventListener("click",changeCircleColor)
+element.addEventListener('mouseover', changeCircleColor);
+// element.addEventListener('mouseout', changeCircleColor2);
+
+ 
+
+// const element=document.querySelector('#modifiedCircle')
+// function changeCircleColor(){
+//     alert(element)
+// }
+// element.addEventListener("click", changeCircleColor)
